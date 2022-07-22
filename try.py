d91271828d9df26ae637ad0e2d99c4639fcea62f
Miharu Namiki <miharu.namiki.5x@gmail.com>
2022-07-22 14:54:47 +0900

    sdfcS

--- try.py
+++ try.py
@@ -1,3 +1,22 @@
-n, x = map(int,input().split())
-ans = ord('A') + x//n - 1
-print(chr(ans)) 
+n,m,s,t = map(int,input().split())
+ab = [map(int,input().split()) for i in range(m)]
+a,b = [list(i) for i in zip(*ab)]
+
+G = [[]for i in range(n)]
+
+for i in range(m):
+    G[a[i]].append(b[i])
+visited = [0] *n
+
+
+def rec(v):
+    visited[v] = 1
+    for next_v in G[v]:
+        if visited[next_v]:
+            continue
+        rec(next_v)
+rec(s)
+if visited[t] == 1:
+    print("Yes")
+else:
+    print("No") 
