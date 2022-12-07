b59b5382d8b221d3e81cd3466baa5f77a0e0002d
Miharu Namiki <miharu.namiki.5x@gmail.com>
2022-12-07 13:57:19 +0900

    a

--- AtCoder/ABC/ABC280/e.cpp
+++ AtCoder/ABC/ABC280/e.cpp
@@ -5,14 +5,13 @@ using mint = atcoder::modint998244353;
 int main() {
 	int n,p;
 	cin>>n>>p;
-    vector dp(n + 1, mint(0));
-    mint m_inv = mint(p) / mint(100) ;
+  vector<mint> dp(n + 1, mint(0));
+  mint m_inv = mint(p) / mint(100) ;
 	mint m_inv2 = 1 - m_inv ;
 
 	dp[0] = 0;
-
-    for(int i = 1; i <=n ; i++) {
-		dp[i] = (dp[max(0,i-2)] * m_inv + dp[i-1]*m_inv2 )+ 1;
-    }
-    cout << dp[n].val() << endl;
+  for(int i = 1; i<=n;i++){
+    dp[i] = dp[i-1]*m_inv2 + dp[max(0,i-2)] * m_inv + 1;
+  }
+  cout << dp[n].val() << endl;
 }
