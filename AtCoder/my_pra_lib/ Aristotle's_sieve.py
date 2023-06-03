b5ac209b5dee7cf6e8717d838be867f13c4cae07
Miharu Namiki <miharu.namiki.5x@gmail.com>
2023-06-03 09:30:55 +0900

    jhgjhg

---
+++ AtCoder/my_pra_lib/ Aristotle's_sieve.py
@@ -0,0 +1,15 @@
+def get_sieve_of_eratosthenes(n):
+    import math
+    if not isinstance(n, int):
+        raise TypeError('n is int type.')
+    if n < 2:
+        raise ValueError('n is more than 2')
+    prime = []
+    limit = math.sqrt(n)
+    data = [i + 1 for i in range(1, n)]
+    while True:
+        p = data[0]
+        if limit <= p:
+            return prime + data
+        prime.append(p)
+        data = [e for e in data if e % p != 0]
